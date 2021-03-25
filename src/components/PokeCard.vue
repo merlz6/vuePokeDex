9672fa06b12f72d66fd2907ee1425eb3c540ae06
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2021-03-25 12:14:13 -0400

    router links api fetch working for show page

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -1,16 +1,73 @@
 <template>
-  <div class="hello">
-    This is the PokeCard Component
-    {{$route.params.name}}
+  <div class="post">
+    <div v-if="loading" class="loading">
+      Loading...
+    </div>
+
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+
+    <div v-if="singlePokemon" class="content">
+      <h2>{{singlePokemon.name}}</h2>
+
+    </div>
   </div>
+
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+
   name: 'PokeCard',
   props: {
     pokemon: {}
-  }
+  },
+  data(){
+    return{
+    singlePokemon:'',
+    loading:false,
+    error:null
+  }},
+  created () {
+  // fetch the data when the view is created and the data is
+  // already being observed
+  this.fetchData()
+  },
+  watch: {
+  // call again the method if the route changes
+  '$route': 'fetchData'
+},
+methods:{
+  fetchData () {
+        this.error = this.post = null
+        this.loading = true
+        const fetchedId = this.$route.params.name
+        // replace `getPost` with your data fetching util / API wrapper
+        // axios.get(`https://pokeapi.co/api/v2/pokemon/` + fetchedId, (err, post) => {
+        //   // make sure this request is the last one we did, discard otherwise
+        //   if (this.$route.params.name !== fetchedId) return
+        //   this.loading = false
+        //   if (err) {
+        //     this.error = err.toString()
+        //   } else {
+        //     this.singlePokemon = post
+        //   }
+        // })
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${fetchedId}`)
+          .then(response => {
+            this.singlePokemon = response.data
+            this.loading = false
+            console.log(this.singlePokemon)
+          })
+          .catch(error => this.error = error)
+      }
+}
+  //  mounted(){
+  //   //
+  //   //   .then(response => (console.log(response)))
+  // }
 }
 </script>
 
